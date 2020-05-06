b4e0b6151453c6d044e4bfa564e2afa612109c40
TaigaU121 <taiga.uji0121@gmail.com>
2020-05-06 21:25:43 +0900

    Deleting files that don't need to be tracked.

--- untracked/.scripts/nuxt-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "overwrite layout/default.vue pages/index.vue"
-cp ~/.scripts/files/nuxt/default.vue ./layouts/default.vue
-cp ~/.scripts/files/nuxt/index.vue ./pages/index.vue
-
-echo "remove unnecessary files"
-rm pages/inspire.vue components/Logo.vue components/VuetifyLogo.vue static/v.png
